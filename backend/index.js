ef8896f97d29cb04972c728e8c71f44ed057018a
A2710 <adityaagarwal2710@gmail.com>
2024-06-04 21:11:51 +0530

    backend update

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.post("/todo", function(req, res){
+
+})
+
+app.get("/todos", function(req, res){
+
+})
+
+app.put("/completed", function(req, res){
+
+})
+
+app.listen(port, function() {
+    console.log(`Example app listening on port http://localhost:3000/.`)
+})
