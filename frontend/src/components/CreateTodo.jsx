ff18855cd7eb3c17b6b1ab3a230cbb0543cad63c
A2710 <adityaagarwal2710@gmail.com>
2024-06-05 23:46:04 +0530

    enable to add a todo to backend using frontend inputs

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,18 +1,43 @@
+import { useState } from "react";
 
-export function CreateTodo(){
+export function CreateTodo(props){
+
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
     return <div>
         <input style={{
             padding: 10,
             margin: 10
-        }} type="text" placeholder="title"/><br />
+        }} type="text" placeholder="title" onChange={function(e){
+            setTitle(e.target.value);
+        }}/><br />
         <input style={{
             padding: 10,
             margin: 10
-        }}  type="text" placeholder="description"/><br />
+        }}  type="text" placeholder="description" onChange={function(e){
+            setDescription(e.target.value);
+        }}/><br />
 
         <button style={{
             padding: 10,
             margin: 10
-        }} >Add a todo</button>
+        }} onClick={() => {
+            //would be easier with axios, but as we are using fetch we have to do it as follows
+
+            fetch("http://localhost:3000/todo", {
+                method: "POST",
+                body: JSON.stringify({
+                    "title" : title,
+                    "description" : description
+                }),
+                headers: {
+                    "content-type": "application/json"
+                }
+            }).then(async function(res){
+                const json = await res.json();
+                alert("Todo added");
+            })
+        }}>Add a todo</button>
     </div>
 }
